8584b831997d9c4122471355448bb11cbcb01d74
Shahrukh <shahrukh1494@gmail.com>
2024-03-07 16:44:01 +0530

    useSelector

--- src/features/cake/CakeView.jsx
+++ src/features/cake/CakeView.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux'
 
 export const CakeView = () => {
     const numOfCakes = useSelector((state) => state.cake.numOfCakes)
+    
     return (
         <div>
             <h2>Number of cakes - {numOfCakes}</h2>
